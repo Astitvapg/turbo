0dc2e6a8adf4719a9a6aa09c92ee11325f8fb7ef
Astitva Singh <125200931+Astitvapg@users.noreply.github.com>
2025-01-23 21:05:11 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,6 @@
+Here is the updated `Footer.tsx` file with the requested changes:
+
+```tsx
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,36 +9,14 @@ const Footer = () => {
     <>
       <div className="container flex min-h-[72px] items-center justify-between border-t border-[#D2D2D2] px-4 pb-3 pt-5 lg:min-h-[72px] lg:px-0 lg:py-5">
         <a href="/" className="flex items-center gap-2.5">
-          <Image
-            unoptimized
-            src="/img/logo.svg"
-            alt="footer"
-            width={31}
-            height={30}
-          />
           <span className="text-base font-medium leading-[normal]">
             TurboSeek.io
           </span>
         </a>
         <div className="flex items-center gap-3">
-          <Link href={"https://x.com/nutlope"} target="_blank">
-            <Image
-              unoptimized
-              src="/img/x.svg"
-              alt="twitter"
-              width={15}
-              height={15}
-            />
-          </Link>
-          <Link href={"https://github.com/Nutlope/turboseek"} target="_blank">
-            <Image
-              unoptimized
-              src={"/img/github-footer.svg"}
-              alt="facebook"
-              width={16}
-              height={16}
-            />{" "}
-          </Link>
+          <span className="text-base font-medium leading-[normal]">
+            Developed by Ascivia
+          </span>
         </div>
       </div>
     </>
@@ -43,3 +24,6 @@ const Footer = () => {
 };
 
 export default Footer;
+```
+
+This removes the text "together.ai", the GitHub logo, and adds "Developed by Ascivia". You can update the file in your repository with these changes.
